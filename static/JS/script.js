b1648e756d12b260672ba35b8bb02f62af6e73dd
Rajat Pandey <44925623+irajatpandey@users.noreply.github.com>
2020-12-16 00:50:36 +0530

    Add files via upload

---
+++ static/JS/script.js
@@ -0,0 +1,18 @@
+//alert('sundi')
+function calculate(){
+  var birthYear = prompt('Enter your Birth Year');
+  var days = (2020 - birthYear) * 365;
+  console.log(days);
+
+  var h1 = document.createElement('h1');
+  var textAns = document.createTextNode('You are ' + days + ' old');
+  h1.setAttribute('id', 'calculate');
+  h1.appendChild(textAns);
+  document.getElementById('result').appendChild(h1);
+  console.log(days);
+}
+
+
+function reset(){
+  document.getElementById('calculate').remove();
+}
